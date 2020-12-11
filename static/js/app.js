299fbefc32e54c585e1695b33970ab2c125e99c7
Shelly Nixon <shellymnixon@gmail.com>
2020-12-11 16:13:06 -0500

    filters

--- static/js/app.js
+++ static/js/app.js
@@ -14,4 +14,39 @@ data.forEach((UFOReport) => {
       cell.text(value);
     });
 });
+
+var button = d3.select("#filter-btn");
+
+
+var form = d3.select("#form");
+
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+
+function runEnter() {
+
   
+  d3.event.preventDefault();
+  
+  
+  var inputElement = d3.select("#datetime");
+
+  
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  tbody.html("")
+  console.log(filteredData);
+  filteredData.forEach((UFOReport) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+};
